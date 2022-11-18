c401af8279b180470a7f479b39de3f3a31bb2eff
Belkin Mike <bel.kin1667@gmail.com>
2022-11-19 00:42:29 +0100

    Initial commit

---
+++ src/main/java/com/l2lhackathon/p2pbot/P2pbotApplication.java
@@ -0,0 +1,13 @@
+package com.l2lhackathon.p2pbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2lhackathon/p2pbot/P2pbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2lhackathon.p2pbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
