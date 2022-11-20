b4fa48f070961fb2aa48b349404b0e40d9a48b73
Belkin Mike <bel.kin1667@gmail.com>
2022-11-20 13:31:44 +0100

    CORS

--- src/main/java/com/l2lhackathon/peers/configuration/SecurityConfiguration.java
+++ src/main/java/com/l2lhackathon/peers/configuration/SecurityConfiguration.java
@@ -4,13 +4,25 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-public class SecurityConfiguration {
-/*    @Bean
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://dmaworks.com", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://radiant-selkie-4b61f9.netlify.app", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "HEAD", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("content-type", "set-cookie", "Authorization", "Access-Control-Allow-Credentials", "sec-fetch-site", "Origin"));
         configuration.setExposedHeaders(Collections.singletonList("sec-fetch-site"));
@@ -23,23 +35,15 @@ public class SecurityConfiguration {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors()
+                .and()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/*").permitAll()
-                .and()
-                .logout()
-                .deleteCookies("session_id", "Authorization")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("https://dmaworks.com/auth/login")
-                .and()
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-    }*/
+                .antMatchers("/*").permitAll();
+    }
 }
 
 
